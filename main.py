3ea72d588403552d420d1d6fa1a7b84d2c267bd4
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-07-31 11:51:09 +0700

    setup again, add candle graph

---
+++ main.py
@@ -0,0 +1,1 @@
+execfile('src/stock_web.py')
--- src/stock_web.py
+++ src/stock_web.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
+# stock_web.py
 
-from dash import dcc, html, Dash
+from http import server
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
 from datetime import datetime
 from src.settings import LOGIN, PASSWORD, SERVER
+import plotly.graph_objects as go
 
 print(LOGIN, PASSWORD, SERVER)
 try:
@@ -14,9 +20,9 @@ try:
     mt.initialize()
     login = LOGIN
     password = PASSWORD
-    server = SERVER
+    server_name = SERVER
 
-    mt.login(login, password, server)
+    mt.login(login, password, server_name)
 
     account_info = mt.account_info()
     print(account_info)
@@ -24,7 +30,7 @@ try:
 except ImportError:
     df_stock = pd.read_csv("../data/EURUSD_D1.csv")
 
-app = Dash()
+app = dash.Dash()
 server = app.server
 
 app.layout = html.Div([
@@ -53,7 +59,7 @@ app.layout = html.Div([
                             )
                         ],
                         "layout": go.Layout(
-                            title='scatter line',
+                            title='Line graph',
                             xaxis={'title': 'Timestamp'},
                             yaxis={'title': 'Closing Rate'}
                         )
@@ -61,9 +67,39 @@ app.layout = html.Div([
                 ),
             ])
         ]),
+        dcc.Tab(label='Candle stock Data', children=[
+            html.Div([
+                html.H4('Stock candlestick chart'),
+                dcc.Checklist(
+                    id='toggle-rangeslider',
+                    options=[{'label': 'Include Rangeslider', 
+                            'value': 'slider'}],
+                    value=['slider']
+                ),
+                dcc.Graph(id="graph"),
+            ])
+        ])
     ])
 ])
 
+@app.callback(
+    Output("graph", "figure"), 
+    Input("toggle-rangeslider", "value"))
+def display_candlestick(value):
+    df_stock = pd.DataFrame(mt.copy_rates_range("EURUSD", mt.TIMEFRAME_D1, datetime(2022, 1, 1), datetime.now()))
+    fig = go.Figure(go.Candlestick(
+        x=df_stock['time'],
+        open=df_stock['open'],
+        high=df_stock['high'],
+        low=df_stock['low'],
+        close=df_stock['close']
+    ))
+
+    fig.update_layout(
+        xaxis_rangeslider_visible='slider' in value
+    )
+
+    return fig
+
 if __name__ == '__main__':
     app.run(debug=True)
-    pass
